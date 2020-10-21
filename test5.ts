d4b27d354bde019765cfbad470e900cdb260eefd
wangliyan <wangliyan@wangliyans-iMac.local>
2020-10-21 11:30:15 +0800

    1

--- test5.ts
+++ test5.ts
@@ -1,4 +1,22 @@
-console.log("123");
-console.log("masteradd");
+// export class ExpandedMenuItemData {
+//   public curPath: string; // 记录当前要进行页面加载的分支路径
+//   public paths: Array<string> = [];
+//   public permissionSettings: number;
+//   public normalIcon: string;
+//   public isWarning = false;
+//   public menuName: string;
 
-console.log("xinfenzhi 111");
+//   constructor(menuName: string) {
+//     this.menuName = menuName;
+//   }
+
+//   public get menuId(): string {
+//     return this.menuName + this.paths.toString();
+//   }
+// }
+
+// const a = new ExpandedMenuItemData("haha");
+// a.normalIcon = "kkkk";
+// console.log(a.normalIcon);
+
+console.log(1 || 2);
