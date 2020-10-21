499db01417daa37c71e4249e1e68c17a746a1ad3
wangliyan <wangliyan@wangliyans-iMac.local>
2020-10-21 11:56:30 +0800

    1

--- test5.ts
+++ test5.ts
@@ -2,3 +2,8 @@ console.log("123");
 console.log("masteradd");
 
 console.log("xinfenzhi 111");
+console.log("masteradd");
+console.log("masteradd");
+console.log("masteradd");
+console.log("masteradd");
+console.log("masteradd");
